f925acf3877e143b6e5b0b905ce90d412e2a0a86
ScottSILS <smurray@sasklibraries.ca>
2014-10-01 09:28:30 -0600

    now with functions!

--- ner.py
+++ ner.py
@@ -1,14 +1,17 @@
 import nltk
 import mods_article
 
-article = mods_article.get_mods_article_text('sample_data.mods.xml')
-tokens = nltk.word_tokenize(article)
-tagged = nltk.pos_tag(tokens)
-entities = nltk.chunk.ne_chunk(tagged)
+def get_ne_persons(mods_xml_article):
+    article_text = mods_article.get_mods_article_text(mods_xml_article)
+    tokens = nltk.word_tokenize(article_text)
+    tagged = nltk.pos_tag(tokens)
+    entities = nltk.chunk.ne_chunk(tagged)
 
-for entity in entities:
-    if isinstance(entity, nltk.tree.Tree):
-        label = entity.label()
-        if label == 'PERSON':
-            for person in entity.leaves():
-                print person[0]
+    for entity in entities:
+        if isinstance(entity, nltk.tree.Tree):
+            label = entity.label()
+            if label == 'PERSON':
+                for person in entity.leaves():
+                    print person[0]
+
+get_ne_persons('sample_data.mods.xml')
