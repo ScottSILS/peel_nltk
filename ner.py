bcd2cc8747c5b34e6ed3e5f6a1e733d77cf780c2
ScottSILS <smurray@sasklibraries.ca>
2014-10-02 12:27:49 -0600

    hopefully fixed get_ne_persons() loop to not fail on badly formed xml

--- ner.py
+++ ner.py
@@ -11,16 +11,18 @@ def find_files(directory, pattern):
 
 def get_ne_persons(mods_xml_article):
     article_text = mods_article.get_mods_article_text(mods_xml_article)
-    tokens = nltk.word_tokenize(article_text)
-    tagged = nltk.pos_tag(tokens)
-    entities = nltk.chunk.ne_chunk(tagged)
+    if article_text:
+        tokens = nltk.word_tokenize(article_text)
+        tagged = nltk.pos_tag(tokens)
+        entities = nltk.chunk.ne_chunk(tagged)
 
-    for entity in entities:
-        if isinstance(entity, nltk.tree.Tree):
-            label = entity.label()
-            if label == 'PERSON':
-                for person in entity.leaves():
-                    print person[0]
+        for entity in entities:
+            if isinstance(entity, nltk.tree.Tree):
+                label = entity.label()
+                if label == 'PERSON':
+                    for person in entity.leaves():
+                        print person[0]
 
 for mods_xml_article in find_files('data', 'Ar*.mods.xml'):
     get_ne_persons(mods_xml_article)
+    print mods_xml_article
